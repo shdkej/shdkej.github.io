dbdd8aa72795e34bb2b3619a45f17edea636b35c
seongho-noh <seongho_noh@kolon.com>
2024-03-20 23:20:52 +0900

    ga4 추가

--- src/html.js
+++ src/html.js
@@ -2,64 +2,72 @@ import React from "react"
 import PropTypes from "prop-types"
 
 export default function HTML(props) {
-  return (
-    <html {...props.htmlAttributes}>
-      <head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
-        <script
-            dangerouslySetInnerHTML={{
-            __html: `
+    return (
+        <html {...props.htmlAttributes}>
+            <head>
+                <meta charSet="utf-8" />
+                <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1, shrink-to-fit=no"
+                />
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
             (function(c,l,a,r,i,t,y){
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
             })(window, document, "clarity", "script", "446ekw0kl6");
-            `,}}
-        />
-        <script
-            key="adsense"
-            data-ad-client="ca-pub-8002857692825455"
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-        />
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-GPKPTMNVF5"></script>
-        <script>
-            dangerouslySetInnerHTML={{
-            __html: `
+            `,
+                    }}
+                />
+                <script
+                    key="adsense"
+                    data-ad-client="ca-pub-8002857692825455"
+                    async
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+                />
+                <script
+                    async
+                    src="https://www.googletagmanager.com/gtag/js?id=G-GPKPTMNVF5"
+                ></script>
+                <script>
+                    dangerouslySetInnerHTML=
+                    {{
+                        __html: `
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
           gtag('config', 'G-GPKPTMNVF5');
-          `
-        </script>
-        <meta name="naver-site-verification" content="0738293103ffff9ea9eb43b3c9621e5fb353f97b" />
-        {props.headComponents}
-      </head>
-      <body {...props.bodyAttributes}>
-        {props.preBodyComponents}
-        <div
-          key={`body`}
-          id="___gatsby"
-          dangerouslySetInnerHTML={{ __html: props.body }}
-        />
-        {props.postBodyComponents}
-      </body>
-    </html>
-  )
+          `,
+                    }}
+                </script>
+                <meta
+                    name="naver-site-verification"
+                    content="0738293103ffff9ea9eb43b3c9621e5fb353f97b"
+                />
+                {props.headComponents}
+            </head>
+            <body {...props.bodyAttributes}>
+                {props.preBodyComponents}
+                <div
+                    key={`body`}
+                    id="___gatsby"
+                    dangerouslySetInnerHTML={{ __html: props.body }}
+                />
+                {props.postBodyComponents}
+            </body>
+        </html>
+    )
 }
 
 HTML.propTypes = {
-  htmlAttributes: PropTypes.object,
-  headComponents: PropTypes.array,
-  bodyAttributes: PropTypes.object,
-  preBodyComponents: PropTypes.array,
-  body: PropTypes.string,
-  postBodyComponents: PropTypes.array,
+    htmlAttributes: PropTypes.object,
+    headComponents: PropTypes.array,
+    bodyAttributes: PropTypes.object,
+    preBodyComponents: PropTypes.array,
+    body: PropTypes.string,
+    postBodyComponents: PropTypes.array,
 }
