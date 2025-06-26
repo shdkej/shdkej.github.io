ee3bae9532ee545b49a1e879dd6cadb05be1cc6d
seongho-noh <seongho_noh@kolon.com>
2025-06-26 10:07:12 +0900

    fix: Layout 불필요 인자 제거

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,40 +3,40 @@ import { Link } from "gatsby"
 
 import "./layout.css"
 import ClientOnly from "./ClientOnly"
-import Search from "./search";
+import Search from "./search"
 
-const Layout = ({ title, children, data }) => {
-    let header
-    header = (
-        <nav>
-            <Link to={`/`}>{title}</Link>
-            <> | </>
-            <Link to={`/tags`}>INDEX</Link>
-            <> | </>
-            <Link to={`/about`}>ABOUT</Link>
-        </nav>
-    )
+const Layout = ({ title, children }) => {
+  let header
+  header = (
+    <nav>
+      <Link to={`/`}>{title}</Link>
+      <> | </>
+      <Link to={`/tags`}>INDEX</Link>
+      <> | </>
+      <Link to={`/about`}>ABOUT</Link>
+    </nav>
+  )
 
-    return (
-        <div>
-            <header>{header}</header>
-            <br />
-            ---
-            <main>{children}</main>
-            <ClientOnly>
-                <Search />
-            </ClientOnly>
-            <footer>
-                <small>
-                    © 2020, Built with
-                    {` `}
-                    <a href="https://www.gatsbyjs.org">Gatsby</a>, Inspired by{" "}
-                    <a href="https://johngrib.github.io/">john grib</a> and{" "}
-                    <a href="https://devroom.io">Ariejan de Vroom</a>
-                </small>
-            </footer>
-        </div>
-    )
+  return (
+    <div>
+      <header>{header}</header>
+      <br />
+      ---
+      <main>{children}</main>
+      <ClientOnly>
+        <Search />
+      </ClientOnly>
+      <footer>
+        <small>
+          © 2020, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>, Inspired by{" "}
+          <a href="https://johngrib.github.io/">john grib</a> and{" "}
+          <a href="https://devroom.io">Ariejan de Vroom</a>
+        </small>
+      </footer>
+    </div>
+  )
 }
 
 export default Layout
