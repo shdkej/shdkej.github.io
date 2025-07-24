896cb87061e2d941504efed02736fdc90887a7ee
seongho-noh <seongho_noh@kolon.com>
2025-07-24 23:08:05 +0900

    디자인 변경

--- src/components/search.js
+++ src/components/search.js
@@ -1,16 +1,24 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 const search = () => {
-    return (
-        <div>
-        <Helmet>
-            <script async src="https://cse.google.com/cse.js?cx=006752884906682281203:76xbe9wsd8h"></script>
-        </Helmet>
-        <div
-            className="gcse-search"
-        >
-        </div>
-        </div>
-    )
+  return (
+    <div>
+      <Helmet>
+        <style>{`
+                .gsc-control-cse {
+                    background-color: #2a2a2a !important;
+                    border: none !important;
+                    box-shadow: none !important;
+                    margin-top: 1em !important;
+                }
+            `}</style>
+        <script
+          async
+          src="https://cse.google.com/cse.js?cx=006752884906682281203:76xbe9wsd8h"
+        ></script>
+      </Helmet>
+      <div className="gcse-search"></div>
+    </div>
+  )
 }
 export default search
